86e171927dd7f107f5d40b17a0b0bbfa107434d1
dave-maldonado <david.m.maldonado@gmail.com>
2014-12-30 12:33:50 -0500

    install angular-resource

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery-ujs
 //= require angular
+//= require angular-resource
 //= require angular-file-upload
 //= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
 //= require_tree .
