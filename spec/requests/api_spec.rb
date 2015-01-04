bdc5d9eed5b2f8ae0b8e3298aad6103a235c2f43
dave-maldonado <david.m.maldonado@gmail.com>
2015-01-04 02:38:40 -0500

    skeleton of api_spec

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 
 RSpec.describe 'picture gallery API', type: :request do
 
-  describe '/api/albums' do
+  describe 'GET /api/albums' do
     it 'responds with http status 200' do
       get api_albums_path
       expect(response).to have_http_status(200)
     end
+
+    it 'responds with properly formatted JSON'
 
     it 'returns correct number of entries' do
       FactoryGirl.create_list(:album, 10)
@@ -15,5 +17,49 @@ RSpec.describe 'picture gallery API', type: :request do
       json = JSON.parse(response.body)
       expect(json.length).to eq(10)
     end
+  end
+
+  describe 'GET /api/albums/1' do
+    it 'responds with http status 200'
+
+    it 'responds with properly formatted JSON'
+  end
+
+  describe 'POST /api/albums' do
+    it 'responds with http status 201'
+  end
+
+  describe 'PATCH /api/albums' do
+    it 'responds with http status 204'
+  end
+
+  describe 'DELETE /api/albums/1' do
+    it 'responds with http status 204'
+  end
+
+  describe 'GET /api/albums/1/pictures' do
+    it 'responds with http status 200'
+
+    it 'responds with properly formatted JSON'
+
+    it 'returns correct number of entries'
+  end
+
+  describe 'GET /api/albums/1/pictures/1' do
+    it 'responds with http status 200'
+
+    it 'responds with properly formatted JSON'
+  end
+
+  describe 'POST /api/albums/1/pictures' do
+    it 'responds with http status 201'
+  end
+
+  describe 'PATCH /api/albums/1/pictures/1' do
+    it 'responds with http status 204'
+  end
+
+  describe 'DELETE /api/albums/1/pictures/1' do
+    it 'responds with http status 204'
   end
 end
